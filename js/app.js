0265ccc0fa1c48ca7b73e6b635dcdac064a8109d
basit188 <basit188@yahoo.com>
2019-03-11 21:04:15 +0530

    OfflineSupport
    OfflineSupport

---
+++ js/app.js
@@ -0,0 +1,19 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load',function(){
+    console.log('here');
+    navigator.serviceWorker.register('../sw_pages.js').then(function(reg) {
+
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
+
+    }).catch(function(error) {
+      // registration failed
+      console.log('Registration failed with ' + error);
+    });
+  });
+}
