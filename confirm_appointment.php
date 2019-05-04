e81036cf754e790af5a7b140f09e6f472449871c
basit188 <basit188@yahoo.com>
2019-05-04 12:20:28 +0530

    minimum time gap in shifts
    minimum time gap in shifts

--- confirm_appointment.php
+++ confirm_appointment.php
@@ -8,16 +8,16 @@ if(isset($_SESSION['login_user']) && isset($_SESSION['user_type']) && $_SESSION[
   $tmp_id = isset($_GET['tmp_id'])?$_GET['tmp_id']:null;
   $confirmed = isset($_GET['confirmed'])? mysqli_real_escape_string($conn,$_GET['confirmed']):null;
   $doc_id = isset($_SESSION['id'])?$_SESSION['id']:null;
-  
+
 
   if($doc_id!=null && $tmp_id!=null){
-    
+
     $result = $conn->query("select patients FROM vw_doctor WHERE doc_id=$doc_id");
     $row = mysqli_fetch_assoc($result);
       $patients = $row['patients'];
-      
-    
-    
+
+
+
     if($confirmed==0){
       if($result = $conn->query("DELETE FROM tmp_appointment WHERE tmp_id=$tmp_id")){
 		  $_SESSION['msg'] ='<div class="alert alert-danger alert-dismissible">
@@ -45,13 +45,13 @@ if(isset($_SESSION['login_user']) && isset($_SESSION['user_type']) && $_SESSION[
       $date = $row['appt_date'];
       $shift = $row['shift'];
       $patient_name = $row['name'];
-      
+
       // get patient details
           $result = $conn->query("select user_email FROM vw_patient WHERE user_id=$pat_id");
     $row = mysqli_fetch_assoc($result);
       $email = $row['user_email'];
-      
-      
+
+
 
       if(bookAppointment($doc_id,$pat_id,new DateTime($date),$shift,$patient_name) && ($result = $conn->query("DELETE FROM tmp_appointment WHERE tmp_id=$tmp_id")))
 	  {
@@ -94,7 +94,7 @@ function bookAppointment($doc_id,$pat_id,$date,$shift,$patient_name){
     $queue_no = slotsFilled($doc_id,$date,$shift)+1;
     $str = "INSERT INTO tb_appointment(doc_id,pat_id,appt_date,name,queue_no,shift) VALUES".
     "($doc_id,$pat_id,'".$date->format('Y-m-d')."','$patient_name',$queue_no,$shift)";
-     
+
 
     $result = $conn->query($str);
     if(!$result){
@@ -148,7 +148,7 @@ function slotsFilled($doc_id,$date,$shift){
 //  /TODO: change to acutal address from localhost
   function send_email($user_email){
     global $conn;
-    
+
    $to      = $user_email; // Send email to our user
 $message = ' Appointmet has been confirmed please visit the dashboard for details';
   require './phpmailer/PHPMailerAutoload.php';
@@ -157,21 +157,21 @@ $message = ' Appointmet has been confirmed please visit the dashboard for detail
 $mail = new PHPMailer;
 
 
-//Enable SMTP debugging. 
-$mail->SMTPDebug = 0;                               
+//Enable SMTP debugging.
+$mail->SMTPDebug = 0;
 //Set PHPMailer to use SMTP.
-$mail->isSMTP();            
-//Set SMTP host name                          
+$mail->isSMTP();
+//Set SMTP host name
 $mail->Host = "smtp.gmail.com";
 //Set this to true if SMTP host requires authentication to send email
-$mail->SMTPAuth = true;                          
-//Provide username and password     
-$mail->Username = "treatme247@gmail.com";                 
-$mail->Password = "Startup@2019"; 
+$mail->SMTPAuth = true;
+//Provide username and password
+$mail->Username = "treatme247@gmail.com";
+$mail->Password = "Startup@2019";
 //If SMTP requires TLS encryption then set it
-$mail->SMTPSecure = "tls";                           
-//Set TCP port to connect to 
-$mail->Port = 587;    
+$mail->SMTPSecure = "tls";
+//Set TCP port to connect to
+$mail->Port = 587;
 $mail->SMTPOptions = array(
     'ssl' => array(
         'verify_peer' => false,
@@ -179,7 +179,7 @@ $mail->SMTPOptions = array(
         'allow_self_signed' => true
     )
 );
-                               
+
 
 
 //From email address and name
@@ -201,16 +201,16 @@ $mail->Subject = "Appointment Confirmed";
 $mail->Body = $message;
 $mail->AltBody = "";
 
-if(!$mail->send()) 
+if(!$mail->send())
 {
 //    echo "Mailer Error: " . $mail->ErrorInfo;
    $_SESSION['msg'] .= '<div class= "alert alert-info alert-dismissible">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     Email Sent</div>';
-  
-  
-} 
-else 
+
+
+}
+else
 {
 //    echo "Message has been sent successfully";
 $_SESSION['msg'] .= '<div class= "alert alert-info alert-dismissible">
@@ -218,11 +218,11 @@ $_SESSION['msg'] .= '<div class= "alert alert-info alert-dismissible">
     Email Sent</div>';
 }
 
-  }
-  
-  
-  
-  
+}
+
+
+
+
 include("footer.php");?>
 </main>
 </html
